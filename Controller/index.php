c08129a4c8688cbaad3518069cdfc62424c6ead8
Jeremy <jihlucky7@gmail.com>
2025-02-26 19:11:27 -0600

    Added comments

--- Controller/index.php
+++ Controller/index.php
@@ -1,7 +1,7 @@
 <?php
-require('../Controller/loadenv.php');
-require('../Model/database.php');
-require('../Model/customer_db.php');
+require('../Controller/loadenv.php'); // Load DB connection variables
+require('../Model/database.php'); // Connect to database
+require('../Model/customer_db.php'); // Load user model
 
 $error_message = ''; // Error messages to be disaplyed on page
 function addErrorText($error) {
@@ -21,43 +21,43 @@ if($action==NULL) {  // If no post request, check for get request
     }
 }
 
-if($action=='login_page') {
+if($action=='login_page') { // Serve login page
     include('../View/login.php');
 }
-else if ($action=="login") {
+else if ($action=="login") { // Handle login form submission
     $uname=filter_input(INPUT_GET, 'username');
     $pass=filter_input(INPUT_GET, 'password');
     $customer=login_cust($uname, $pass); // Login function from customer model
     if($customer==NULL) {
-        $error_message.=addErrorText('Username does not exist or username and password does not match.');
+        $error_message.=addErrorText('Username does not exist or username and password do not match.');
         include('../View/login.php');
-        
     }
     else {
+        // Store values from retrieved user
         $id=$customer[0];
         $username=$customer[1];
         $password=$customer[2];
-        
+        // Set cookies
         setcookie("id", $id, time()+(86400*30), "/");
         setcookie("username", $username, time()+(86400*30), "/");
         setcookie("password", substr($password, 0, 4), time()+(86400*30), "/");
-        
+        // Redirect to login page, which will redirect to customer page
         header('Location: .?action=login_page');
     }
 }
-else if ($action == "signup_page") {
+else if ($action == "signup_page") { // Serve signup page
     include('../View/signup.php');
 }
-else if ($action == "signup") {
+else if ($action == "signup") { // Handle signup for submission
     $email=filter_input(INPUT_GET, "email");
     $uname=filter_input(INPUT_GET, "username");
     $fname=filter_input(INPUT_GET, "firstname");
     $lname=filter_input(INPUT_GET, "lastname");
     $pass1=filter_input(INPUT_GET, "password1");
     $pass2=filter_input(INPUT_GET, "password2");
-    
+    // If there is an error, this value is embedded in the page to tell JavaScript which field to highlight
     $error_cursor = -1;
-
+    // Handle empty fields
     if (empty($email)) {
         $error_message.=addErrorText('Fields may not be empty.');
         $error_cursor = 0;
@@ -82,35 +82,37 @@ else if ($action == "signup") {
         $error_message.=addErrorText('Fields may not be empty.');
         $error_cursor = 5;
     }
-
+    // Check for password mismatch
     if ((!empty($pass1) && !empty($pass2)) && $pass1 != $pass2) {
         $error_message.=addErrorText('Passwords must match.');
         $error_cursor = 4;
     }
-
+    // Check for duplicate email
     if (check_existing("CustEmail", $email)) {
         $error_message.=addErrorText('There is already an account with this email address.');
         $error_cursor = 0;
     }
-
-    
+    // Check for duplicate username
     if (check_existing("CustUserName", $uname)) {
         $error_message.=addErrorText('This username is already taken.');
         if ($error_cursor >= 1)
             $error_cursor = 1;
     }
-
+    // If $error_cursor is the sentinel value, hash password and insert user to DB
     if ($error_cursor == -1) {
-        $pass = password_hash($pass1, PASSWORD_DEFAULT);
-        $status = create_cust($email, $uname, $pass, $fname, $lname);
-        if ($status == true) {
+        $pass = password_hash($pass1, PASSWORD_DEFAULT); // Hash password with default PHP Blowfish algorithm
+        $status = create_cust($email, $uname, $pass, $fname, $lname); // Insert function form customer model
+        if ($status == true) { // After successful insertion, log in the new user
             header('Location: .?action=login&username='.$uname.'&password='.$pass1);
         }
+        else { // ToDo: More robust error logging for if this situation were to occur
+            $error_message.=addErrorText("An error occured on our end. Please try again later.")
+        }
     }
-    else {
+    else { // Serve the signup page again with error messages
         include('../View/signup.php');
     }
-}
+} // Force cookie expiration and boot to login page
 else if ($action == "logout") {
     setcookie("id", "", time()-3600, "/");
     setcookie("username", "", time()-3600, "/");
--- Model/customer_db.php
+++ Model/customer_db.php
@@ -1,15 +1,16 @@
 <?php
+    // Handle login
     function login_cust($u, $p) {
         global $db;
-
+        // Select pased on provided username
         $query='SELECT CustID, CustUserName, CustPassword FROM Customers
-        WHERE CustUserName = :username';
+        WHERE CustUserName = :username'; // Since passwords are hashed and usernames are unique, I query by username only
         $statement=$db->prepare($query);
         $statement->bindValue(':username', $u);
         $statement->execute();
         $cust=$statement->fetch();
         $statement->closeCursor();
-
+        // Check hash against provided password
         $verified = password_verify($p, $cust[2]);
 
         if ($verified)
@@ -17,20 +18,20 @@
         else
             return false;
     }
-
+    // Check if a customer exists with the provided data ($value) in the provided column ($field)
     function check_existing($field, $value) {
         global $db;
         $query='SELECT CustID, CustUserName, CustPassword FROM Customers
-        WHERE '.$field.' = :val';
+        WHERE '.$field.' = :val'; // $field is concatonated rather than binded to avoid putting it in quotes
         $statement=$db->prepare($query);
         $statement->bindValue(':val', $value);
         $statement->execute();
         $cust=$statement->fetch();
         $statement->closeCursor();
 
-        return (bool)$cust;
+        return (bool)$cust; // Just return if the row exists
     }
-
+    // Insert a customer into DB. Used on the signup page
     function create_cust($e, $u, $p, $fn, $ln) {
         global $db;
         $query='INSERT INTO Customers (CustUserName, CustPassword, CustEmail, CustFname, CustLname)
@@ -50,7 +51,7 @@
             return false;
         }
     }
-
+    // Uses the id cashed in a cookie to retrieve customer data which can be used to populate a page
     function retrieve_cust($id) {
         global $db;
         $query = "SELECT CustID, CustUserName, CustEmail, CustFname, CustLname
